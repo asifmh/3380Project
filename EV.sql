e506670b66b3a69c6891605ad9a6d4592fb56fa7
asifmh <106801964+asifmh@users.noreply.github.com>
2023-11-21 11:36:30 -0600

    utility table

--- EV.sql
+++ EV.sql
@@ -5,3 +5,14 @@ CREATE TABLE "vehicle" (
 	"Make"	TEXT NOT NULL,
 	PRIMARY KEY("VIN")
 );
+CREATE TABLE utility
+(
+Utility Id  INTEGER primary key ,
+postal code Text primary key ,
+state text primary key ,
+county text primary key ,
+city text references  location (city),
+utility name  integer NOT NULL,
+PRIMARY KEY (city)
+
+);

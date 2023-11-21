aa185d53de036fec075ae01217511dab4aebe4b3
asifmh <106801964+asifmh@users.noreply.github.com>
2023-11-21 11:47:17 -0600

    utility table

--- EV.sql
+++ EV.sql
@@ -1,56 +1,18 @@
 CREATE TABLE "vehicle" (
 	"VIN"	TEXT NOT NULL,
 	"Model_year"	INTEGER,
-	"Model"	TEXT ,
-	"Make"	TEXT ,
+	"Model"	TEXT NOT NULL,
+	"Make"	TEXT NOT NULL,
 	PRIMARY KEY("VIN")
 );
+CREATE TABLE utility
+(
+Utility Id  INTEGER primary key ,
+postal code Text primary key ,
+state text primary key ,
+county text primary key ,
+city text references  location (city),
+utility name  integer NOT NULL,
+PRIMARY KEY (city)
 
-CREATE TABLE "vehicleDetail" (
-	"VIN"	TEXT,
-	"Model"	TEXT,
-	"Model_year"	INTEGER,
-	"Make"	TEXT,
-	"electRange"	INTEGER,
-	"base_msrp"	INTEGER,
-	FOREIGN KEY("Model") REFERENCES "vehicle"("Model"),
-	FOREIGN KEY("Model_year") REFERENCES "vehicle"("Model_year"),
-	FOREIGN KEY("Make") REFERENCES "vehicle"("Make"),
-	FOREIGN KEY("VIN") REFERENCES "vehicle"("VIN")
 );
-
-CREATE TABLE "altFuel" (
-	"Model"	TEXT,
-	"Model_year"	INTEGER,
-	"Make"	TEXT,
-	"CAFV"	TEXT,
-	FOREIGN KEY("Make") REFERENCES "vehicle"("Make"),
-	FOREIGN KEY("Model") REFERENCES "vehicle"("Model"),
-	PRIMARY KEY("Model","Model_year","Make"),
-	FOREIGN KEY("Model_year") REFERENCES "vehicle"("Model_year")
-);
-
-CREATE TABLE "location"(
-    "DOL_ID"	INTEGER NOT NULL UNIQUE,
-   	PRIMARY KEY("DOL_ID"),
-   	"longitude" DECIMAL(10, 6),
-   	"latitude" DECIMAL(10, 6),
-   	"State" TEXT,
-   	"County" TEXT,
-   	"City" TEXT,
-   	"Postal_code" INTEGER
-);
-
-CREATE TABLE "District"(
-
-    "DOL_ID" INTEGER,
-    "longitude"  DECIMAL(10, 6),
-    "latitude" DECIMAL(10, 6),
-    FOREIGN KEY("DOL_ID") REFERENCES "location"("DOL_ID"),
-    FOREIGN KEY("longitude") REFERENCES "location"("longitude"),
-    FOREIGN KEY("latitude") REFERENCES "location"("latitude"),
-    "Ld" INTEGER,
-    PRIMARY KEY(DOL_ID, longitude, latitude)
-    );
-
-

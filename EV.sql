c5440ee8f31ee701845b9e23e1729dbcb3cdef63
elroy <fernan47@myumanitoba.ca>
2023-11-21 11:29:17 -0600

    new tables added

--- EV.sql
+++ EV.sql
@@ -1,7 +1,62 @@
 CREATE TABLE "vehicle" (
 	"VIN"	TEXT NOT NULL,
 	"Model_year"	INTEGER,
-	"Model"	TEXT NOT NULL,
-	"Make"	TEXT NOT NULL,
+	"Model"	TEXT ,
+	"Make"	TEXT ,
 	PRIMARY KEY("VIN")
+);
+
+CREATE TABLE "vehicleDetail" (
+	"VIN"	TEXT,
+	"Model"	TEXT,
+	"Model_year"	INTEGER,
+	"Make"	TEXT,
+	"electRange"	INTEGER,
+	"base_msrp"	INTEGER,
+	FOREIGN KEY("Model") REFERENCES "vehicle"("Model"),
+	FOREIGN KEY("Model_year") REFERENCES "vehicle"("Model_year"),
+	FOREIGN KEY("Make") REFERENCES "vehicle"("Make"),
+	FOREIGN KEY("VIN") REFERENCES "vehicle"("VIN")
+);
+
+CREATE TABLE "altFuel" (
+	"Model"	TEXT,
+	"Model_year"	INTEGER,
+	"Make"	TEXT,
+	"CAFV"	TEXT,
+	FOREIGN KEY("Make") REFERENCES "vehicle"("Make"),
+	FOREIGN KEY("Model") REFERENCES "vehicle"("Model"),
+	PRIMARY KEY("Model","Model_year","Make"),
+	FOREIGN KEY("Model_year") REFERENCES "vehicle"("Model_year")
+);
+
+CREATE TABLE "location"(
+    "DOL_ID"	INTEGER NOT NULL UNIQUE,
+   	PRIMARY KEY("DOL_ID"),
+   	"longitude" DECIMAL(10, 6),
+   	"latitude" DECIMAL(10, 6),
+   	"State" TEXT,
+   	"County" TEXT,
+   	"City" TEXT,
+   	"Postal_code" INTEGER
+);
+
+CREATE TABLE "District"(
+
+    "DOL_ID" INTEGER,
+    "longitude"  DECIMAL(10, 6),
+    "latitude" DECIMAL(10, 6),
+    FOREIGN KEY("DOL_ID") REFERENCES "location"("DOL_ID"),
+    FOREIGN KEY("longitude") REFERENCES "location"("longitude"),
+    FOREIGN KEY("latitude") REFERENCES "location"("latitude"),
+    "Ld" INTEGER,
+    PRIMARY KEY(DOL_ID, longitude, latitude)
+    );
+
+
+
+);
+
+
+
 );
